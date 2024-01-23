ac1ecebb09551f4bb18c5cf360d4eaf636ed6dd0
Tommaso Pardi <tommaso.pardi@gmail.com>
2024-01-23 16:11:47 +0000

    feat: added argparse

--- stl2obj.py
+++ stl2obj.py
@@ -1,6 +1,8 @@
 import numpy as np
 from stl import mesh
 from tqdm import tqdm
+import argparse
+import os
 
 def convert_stl_to_obj(stl_file, obj_file):
     # Read STL file
@@ -18,5 +20,37 @@ def convert_stl_to_obj(stl_file, obj_file):
             indices = [np.where((np.unique(stl_mesh.vectors.reshape(-1, 3), axis=0) == vertex).all(axis=1))[0][0] + 1 for vertex in f]
             obj.write(f"f {indices[0]} {indices[1]} {indices[2]}\n")
 
-# Example usage
-convert_stl_to_obj('input/base_link.stl', 'output/output.obj')
+# # Example usage
+# convert_stl_to_obj('input/base_link.stl', 'output/output.obj')
+
+if __name__ == "__main__":
+    
+    parser = argparse.ArgumentParser()
+    parser.add_argument("-v", "--verbose", help="Set the verbosity level", action="store_true")
+    parser.add_argument("-i", "--input", help="Select the directory or file to convert", required=True)
+    parser.add_argument("-o", "--output", help="Select the directory or file to save the output, default is: output/", default="output")
+    args = parser.parse_args()
+    
+    abs_output_path = os.path.join(os.getcwd(), args.output)
+    abs_input_path = os.path.join(os.getcwd(), args.input)
+    
+    # Create output folder
+    if not os.path.isdir(abs_output_path):
+        os.mkdir(abs_output_path)
+    
+    # Obtain the files
+    stl_files = []
+
+    if os.path.isdir(abs_input_path):
+        for f in os.listdir(abs_input_path):
+            if f.endswith('.stl'):
+                stl_files.append(f)
+    elif abs_input_path.endswith('.stl'):
+        stl_files = [abs_input_path]
+        
+    if abs.verbose:
+        print(f'{len(stl_files)} stl files found')
+        
+    for idx, stl_in in tqdm(enumerate(stl_files)):
+        print(f'#{idx} file started')
+        convert_stl_to_obj(os.path.join(abs_input_path, stl_in), os.path.join(abs_output_path, stl_in.removesuffix('.stl') + '.obj'))
